d722d3620de1a478053c8f50dfaac02bb5b12bea
Mehrunissa Pirwani (2303.KHI.CND.007) <mehrunissa.pirwani@xloopdigital.com>
2023-12-31 18:33:24 +0500

    fixing bugs

--- app/api/prompt/new/route.js
+++ app/api/prompt/new/route.js
@@ -1,5 +1,5 @@
-import Prompt from "@models/prompt";
-import { connectToDB } from "@utils/database";
+import Prompt from "../../../../models/prompt";
+import connectToDB from "../../../../models/connect;
 
 export const POST = async (request) => {
     const { userId, prompt, tag } = await request.json();
--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -3,7 +3,7 @@ import { useState, useEffect } from "react"
 import { useSession } from "next-auth/react"
 import { useRouter } from "next/router"
 
-import Profile from '@components/Profile'
+import Profile from "../../components/Profile"
 
 const MyProfile = () => {
    const {data:session} = useSession();
